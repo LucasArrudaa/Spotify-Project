531ae12cf33a21fd6c52e2ba2305406b6214443b
lucas.sarruda <lucassol133@gmail.com>
2025-08-21 07:41:08 -0300

    First commit, usuario adicionado

--- src/Midias.java
+++ src/Midias.java
@@ -1,11 +1,7 @@
 public class Midias {
-
     /// ○ Possui título, artista, duração e gênero.
-    ///
     /// ○ Deve ser armazenada em um catálogo geral de músicas disponíveis.
-    ///
     /// ○ Mídias podem ser musicas, podcasts ou audiobooks
-
     String NomeMusica;
 
     public Midias(String nomeMusica) {
